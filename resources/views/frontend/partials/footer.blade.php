cc5a5000ca1066f821732152c0da8044cb82a5fc
Clovis Amai <clovisamaityan@gmail.com>
2020-08-10 15:31:06 -0700

    zippy txt added

--- resources/views/frontend/partials/footer.blade.php
+++ resources/views/frontend/partials/footer.blade.php
@@ -58,7 +58,7 @@
             @if(isset($footersettings[0]) && $footersettings[0]['footer'])
                 {{ $footersettings[0]['footer'] }}
             @else
-                © 2018 Developer Canvas Studio.
+                © 2020 Cyber Grains
             @endif
 
             @if(isset($footersettings[0]) && $footersettings[0]['facebook'])
